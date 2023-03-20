3f69565a493fdc0b99ede116a42f8bd501adcd67
SkullKid2113 <125101202+SkullKid2113@users.noreply.github.com>
2023-03-19 20:26:48 -0500

    #3 How bout meow

---
+++ src/ammunition/ammunition.js
@@ -0,0 +1,16 @@
+export class Ammunition {
+
+ ammunitionType = ["7.62mm", "9mm"]
+ damagePerBullet = null
+ muzzleVelocity = null
+ friction = null
+ weight = null
+ 
+    constructor(damagePerBullet, muzzleVelocity, friction, weight){
+        this.damagePerBullet = damage
+        this.muzzleVelocity = velocity
+        this.friction = friction
+        this.weight = weight
+    }
+    
+}
--- src/weapon/weapon.js
+++ src/weapon/weapon.js
@@ -34,21 +34,6 @@ export class Weapon {
         this.numRounds = 32
     }
 
-    ammunition(){
-
-        if (this.ammo = "7.62mm")
-            damagePerBullet = 20.8
-            muzzleVelocity = 2.5
-            friction = 3.2
-            weight = .5
-
-         if (this.ammo = "9mm")  
-         damagePerBullet = 15.6
-         muzzleVelocity = 1.8
-         friction = 2.3
-         weight = (.2, "grains")
-    }
-
     changeFireMode(mode) {
         if (this.fireModes.indexOf(mode) === -1) {
             throw {error: "Invalid fire mode"}
