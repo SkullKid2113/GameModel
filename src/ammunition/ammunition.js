64e33c284cd96e0a8afc11f454fba7db2e487806
SkullKid2113 <125101202+SkullKid2113@users.noreply.github.com>
2023-03-19 20:34:54 -0500

    #3 OUCHY MY HEAD

--- src/ammunition/ammunition.js
+++ src/ammunition/ammunition.js
@@ -7,10 +7,24 @@ export class Ammunition {
  weight = null
  
     constructor(damagePerBullet, muzzleVelocity, friction, weight){
-        this.damagePerBullet = damage
-        this.muzzleVelocity = velocity
+        this.damagePerBullet = damagePerBullet
+        this.muzzleVelocity = muzzleVelocity
         this.friction = friction
         this.weight = weight
     }
     
+    Ammunition(){
+        
+        if (this.ammunitionType === "7.62mm")
+        this.damagePerBullet = 43
+        this.muzzleVelocity = 23
+        this.friction = 10
+        this.weight = 300
+
+        if (this.ammunitionType === "9mm")
+        this.damagePerBullet = 30
+        this.muzzleVelocity = 15
+        this.friction = 5
+        this.weight = 148
+    }
 }
