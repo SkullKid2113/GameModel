052eac2409697d67b4ea3bb7d71802014bb6cd23
SkullKid2113 <125101202+SkullKid2113@users.noreply.github.com>
2023-03-21 16:20:26 -0500

    #3 ammunition fix

--- src/ammunition/ammunition.js
+++ src/ammunition/ammunition.js
@@ -1,30 +1,17 @@
 export class Ammunition {
 
-    ammunitionType = ["7.62mm", "9mm"]
+    ammunitionType = []
     damagePerBullet = null
     muzzleVelocity = null
     friction = null
     weight = null
 
     constructor(damagePerBullet, muzzleVelocity, friction, weight) {
+            this.ammunitionType = ammunitionType
         this.damagePerBullet = damagePerBullet
         this.muzzleVelocity = muzzleVelocity
         this.friction = friction
         this.weight = weight
     }
 
-    Bullet() {
-
-        if (this.ammunitionType === "7.62mm")
-            this.damagePerBullet = 43
-        this.muzzleVelocity = 23
-        this.friction = 10
-        this.weight = 300
-
-        if (this.ammunitionType === "9mm")
-            this.damagePerBullet = 30
-        this.muzzleVelocity = 15
-        this.friction = 5
-        this.weight = 148
-    }
-}
+  }
