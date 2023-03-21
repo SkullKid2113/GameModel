cf4310b99ceaf2a4b9e1d310b13e80f9c7e2dc56
SkullKid2113 <125101202+SkullKid2113@users.noreply.github.com>
2023-03-20 19:56:14 -0500

    #4 Player Creation Attempt

--- src/Player/player.js
+++ src/Player/player.js
@@ -8,4 +8,28 @@ yLocation = 0
 moveSpeed = 0
 jumpHeight = 7
 
+    constructor (name, moveSpeed)  {
+        this.name = name 
+        this.moveSpeed = moveSpeed 
+
+        if (setName("")){
+            this.name === "required"
+        }
+
+        if (setRunning()){
+            this.moveSpeed = 30
+        }
+
+        if (setWalking()){
+            this.moveSpeed = 10
+        }
+
+        if (setStopped()){
+            this.moveSpeed = 0
+        }
+
+    }
+
+    
 }
+
