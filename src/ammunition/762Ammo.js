b75b14e4ffce641c5ea6199532ead9398396fc3f
SkullKid2113 <125101202+SkullKid2113@users.noreply.github.com>
2023-03-23 11:12:23 -0500

    #5 7.62 damage to muzzlevelocity

--- src/ammunition/762Ammo.js
+++ src/ammunition/762Ammo.js
@@ -3,9 +3,9 @@ import {Ammunition} from "./ammunition.js";
 export class ammo762 extends Ammunition {
 
   constructor() {
-    super("weight", "damage", "friction", "damagePerBullet", "ammunitionType");
+    super("weight", "muzzleVelocity", "friction", "damagePerBullet", "ammunitionType");
       this.weight = 2
-      this.damage = 4
+      this.muzzleVelocity = 4
       this.friction = 2
       this.damagePerBullet = 8
       this.ammunitionType = "7.62mm"
