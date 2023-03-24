8b59342cc948d96f1a23a3e0e207a40e99f431c9
SkullKid2113 <125101202+SkullKid2113@users.noreply.github.com>
2023-03-23 19:16:14 -0500

    #6 strings changed to parameters

--- src/ammunition/556Ammo.js
+++ src/ammunition/556Ammo.js
@@ -3,11 +3,11 @@ import {Ammunition} from "./ammunition.js";
 export class Ammo556 extends Ammunition {
 
   constructor() {
-    super("weight", "muzzleVelocity", "friction", "damagePerBullet", "ammunitionType");
+    super(weight, muzzleVelocity, friction, damagePerBullet, ammunitionType);
       this.weight = 3
       this.muzzleVelocity = 3
       this.friction = 1
       this.damagePerBullet = 5
       this.ammunitionType = "5.56mm"
     }
-    }
+  }
