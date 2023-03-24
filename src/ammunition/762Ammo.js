de75551d9cfb54af887eb9b3a94158baf0da52cb
SkullKid2113 <125101202+SkullKid2113@users.noreply.github.com>
2023-03-23 19:12:51 -0500

    #5 7.62 strings changed to parameters

--- src/ammunition/762Ammo.js
+++ src/ammunition/762Ammo.js
@@ -3,7 +3,7 @@ import {Ammunition} from "./ammunition.js";
 export class Ammo762 extends Ammunition {
 
   constructor() {
-    super("weight", "muzzleVelocity", "friction", "damagePerBullet", "ammunitionType");
+    super(weight, muzzleVelocity, friction, damagePerBullet, ammunitionType);
       this.weight = 2
       this.muzzleVelocity = 4
       this.friction = 2
