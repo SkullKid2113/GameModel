708d1ed18f2c8dd45c5585f913e16826e0a76f8c
Justin Rovang <rovangju@itnobody.com>
2023-03-28 12:38:06 -0500

    Tweaks

--- src/ammunition/556Ammo.js
+++ src/ammunition/556Ammo.js
@@ -2,12 +2,7 @@ import {Ammunition} from "./ammunition.js";
 
 export class Ammo556 extends Ammunition {
 
-  constructor(damage, damagePer) {
-    super(weight, damage, friction, damagePer, ammunitionType);
-      this.weight = 3
-      this.damage = 27
-      this.friction = 1
-      this.damagePer = 5
-      this.ammunitionType = "5.56mm"
+    constructor() {
+        super(5, 1400, 0.05, 40);
     }
-  }
+}
--- src/ammunition/ammunition.js
+++ src/ammunition/ammunition.js
@@ -1,16 +1,16 @@
 export class Ammunition {
 
-  ammunitionType = ""
-  damagePerBullet = null
-  muzzleVelocity = null
-  friction = null
-  weight = null
+    ammunitionType = ""
+    damagePerBullet = null
+    muzzleVelocity = null
+    friction = null
+    weight = null
 
-  constructor(damagePerBullet, muzzleVelocity, friction, weight) {
-    this.ammunitionType = ammunitionType
-    this.damagePerBullet = damagePerBullet
-    this.muzzleVelocity = muzzleVelocity
-    this.friction = friction
-    this.weight = weight
+    constructor(ammunitionType, damagePerBullet, muzzleVelocity, friction, weight) {
+        this.ammunitionType = ammunitionType
+        this.damagePerBullet = damagePerBullet
+        this.muzzleVelocity = muzzleVelocity
+        this.friction = friction
+        this.weight = weight
     }
-  }
+}
