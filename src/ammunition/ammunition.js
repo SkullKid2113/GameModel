7b6564d62ae973a06dfeaa8b683bdb75d3f2de67
SkullKid2113 <125101202+SkullKid2113@users.noreply.github.com>
2023-03-28 20:58:03 -0500

    ammo

--- src/ammunition/ammunition.js
+++ src/ammunition/ammunition.js
@@ -6,7 +6,7 @@ export class Ammunition {
     friction = null
     weight = null
 
-    constructor(damagePerBullet, muzzleVelocity, friction, weight) {
+    constructor(ammunitionType, damagePerBullet, muzzleVelocity, friction, weight) {
         this.ammunitionType = ammunitionType
         this.damagePerBullet = damagePerBullet
         this.muzzleVelocity = muzzleVelocity
