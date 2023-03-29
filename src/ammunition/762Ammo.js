8fafb486faa45cd3eb347f3db5ef47f66cd97a5d
SkullKid2113 <125101202+SkullKid2113@users.noreply.github.com>
2023-03-28 20:59:01 -0500

    #5 add mm

--- src/ammunition/762Ammo.js
+++ src/ammunition/762Ammo.js
@@ -3,6 +3,6 @@ import {Ammunition} from "./ammunition.js";
 export class Ammo762 extends Ammunition {
 
   constructor() {
-    super("7.62", 7, 1200, 0.07, 55);
+    super("7.62mm", 7, 1200, 0.07, 55);
     }
 }
