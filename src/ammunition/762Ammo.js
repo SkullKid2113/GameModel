f98c77c322d558e541366834f43c0fe9bd7f2466
SkullKid2113 <125101202+SkullKid2113@users.noreply.github.com>
2023-03-28 20:49:01 -0500

    #5 super upgrade

--- src/ammunition/762Ammo.js
+++ src/ammunition/762Ammo.js
@@ -2,12 +2,7 @@ import {Ammunition} from "./ammunition.js";
 
 export class Ammo762 extends Ammunition {
 
-  constructor(damage, damagePer) {
-    super(weight, damage, friction, damagePer, ammunitionType);
-      this.weight = 2
-      this.damage = 32
-      this.friction = 2
-      this.damagePer = 8
-      this.ammunitionType = "7.62mm"
+  constructor() {
+    super(7, 1200, 0.07, 55);
     }
 }
