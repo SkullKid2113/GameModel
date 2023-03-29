2aaa346f489e064971c96cb403850175388bb197
Justin Rovang <rovangju@itnobody.com>
2023-03-29 11:46:24 -0500

    Fixies

--- src/character/player.js
+++ src/character/player.js
@@ -1,39 +1,37 @@
-import { Ak47 } from "../weapon/ak47.js";
-import {Weapon} from "../weapon/weapon.js";
-
 export class Player {
 
-  name = null 
-  weaponSlots =[]
-  ammoSlots = []
-  xLocation = 0 
-  yLocation = 0 
-  moveSpeed = 0 
-  jumpHeight = 7 
+    name = null
+    weaponSlots = []
+    ammoSlots = []
+    xLocation = 0
+    yLocation = 0
+    moveSpeed = 0
+    jumpHeight = 7
 
-  constructor (name)  {
-    this.name = name
-  }
-
-  setRunning() {
-      this.moveSpeed = 30 
-  }
-
-  setWalking() {
-      this.moveSpeed = 10
-  }
-
-  setStopped() {
-      this.moveSpeed = 0
-  }
-
-  equipWeapon(weapon) {
-
-   if(this.equipWeapon && this.weaponSlots < 1){
-      this.weaponSlots +=1
-      
+    constructor(name) {
+        this.name = name
     }
-  }
 
+    setRunning() {
+        this.moveSpeed = 30
+    }
+
+    setWalking() {
+        this.moveSpeed = 10
+    }
+
+    setStopped() {
+        this.moveSpeed = 0
+    }
+
+    equipWeapon(weapon) {
+
+        const hasThisWeaponAlready = this.weaponSlots.filter(
+            weaponAlreadyEquipped => weaponAlreadyEquipped.name === weapon.name
+        )
+
+        if (hasThisWeaponAlready.length === 0) {
+            this.weaponSlots.push(weapon)
+        }
+    }
 }
-
